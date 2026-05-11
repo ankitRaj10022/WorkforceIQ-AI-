2e9c1dbabd1672ee70729deb054b83f875ff5b7f
Ankit Raj <dannyloyad149@gmail.com>
2026-05-11 10:13:37 +0530

    c3

--- workforceiq/celery_app.py
+++ workforceiq/celery_app.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import ssl
+
 from celery import Celery
 
 from workforceiq import create_app
@@ -12,14 +14,20 @@ def make_celery() -> Celery:
         broker=flask_app.config["CELERY_BROKER_URL"],
         backend=flask_app.config["CELERY_RESULT_BACKEND"],
     )
-    celery_app.conf.update(
-        imports=("workforceiq.tasks",),
-        task_serializer="json",
-        result_serializer="json",
-        accept_content=["json"],
-        timezone="UTC",
-        enable_utc=True,
-    )
+    config = {
+        "imports": ("workforceiq.tasks",),
+        "task_serializer": "json",
+        "result_serializer": "json",
+        "accept_content": ["json"],
+        "timezone": "UTC",
+        "enable_utc": True,
+    }
+    if flask_app.config["CELERY_BROKER_URL"].startswith("rediss://"):
+        config["broker_use_ssl"] = {"ssl_cert_reqs": ssl.CERT_REQUIRED}
+    if flask_app.config["CELERY_RESULT_BACKEND"].startswith("rediss://"):
+        config["redis_backend_use_ssl"] = {"ssl_cert_reqs": ssl.CERT_REQUIRED}
+
+    celery_app.conf.update(**config)
 
     class FlaskContextTask(celery_app.Task):
         def __call__(self, *args, **kwargs):
